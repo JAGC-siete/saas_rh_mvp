2a2473013d4eabc858f27b15959617b4133a0bce
JAGC-siete <jorge7gomez@gmail.com>
2025-10-04 10:54:44 -0600

    validaciones corregidas y valudation auth corrected as well

--- pages/api/user-profiles.ts
+++ pages/api/user-profiles.ts
@@ -1,5 +1,5 @@
 import { NextApiRequest, NextApiResponse } from 'next'
-import { createClient } from '../../../lib/supabase/server'
+import { createClient } from '../../lib/supabase/server'
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method !== 'GET') {
